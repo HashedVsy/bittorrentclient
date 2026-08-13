d476ba8e183d2ae1557184a5353b8d3a533b98fe
Hashed <bilal.karabacak52@icloud.com>
2026-08-13 13:10:50 +0200

    Add production torrent engine C API

---
+++ src/engine.h
@@ -0,0 +1,10 @@
+#ifndef BT_ENGINE_H
+#define BT_ENGINE_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+int bt_engine_run(const char *input, const char *save_path);
+#ifdef __cplusplus
+}
+#endif
+#endif
