55faf464a498675f411e3dd8ef09d49a0792ec86
Hashed <bilal.karabacak52@icloud.com>
2026-08-13 12:44:22 +0200

    Add DHT constants and persistence configuration

--- src/common.h
+++ src/common.h
@@ -9,8 +9,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define BT_VERSION "0.1.0"
+#include <time.h>
+#define BT_VERSION "0.2.0"
 #define BT_PORT 6882
+#define DHT_PORT 6881
+#define DHT_K 8
+#define DHT_ALPHA 3
+#define DHT_TIMEOUT_MS 1500
+#define DHT_MAX_NODES 512
+#define DHT_MAX_BUCKETS 160
+#define DHT_NODE_FILE "dht.nodes"
 #define BLOCK_SIZE 16384
 #define MAX_PEERS 512
 typedef struct { char ip[INET6_ADDRSTRLEN]; uint16_t port; } PeerAddress;
